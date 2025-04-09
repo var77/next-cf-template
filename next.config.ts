38927b7755b52090ec470540ad9e17e4cfc44b52
Varik Matevosyan <varikmatevosyan@gmail.com>
2025-04-09 15:58:11 +0400

    initial blank template

---
+++ next.config.ts
@@ -0,0 +1,27 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
+// Here we use the @cloudflare/next-on-pages next-dev module to allow us to
+// use bindings during local development (when running the application with
+// `next dev`). This function is only necessary during development and
+// has no impact outside of that. For more information see:
+// https://github.com/cloudflare/next-on-pages/blob/main/internal-packages/next-dev/README.md
+if (process.env.NODE_ENV === 'development') {
+  setupDevPlatform().catch(console.error);;
+}
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+  }, /* config options here */
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
