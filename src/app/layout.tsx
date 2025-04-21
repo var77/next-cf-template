8b320312b8a7b9603c772c780ebef7ca9ef1e4f7
Varik Matevosyan <varikmatevosyan@gmail.com>
2025-04-21 15:41:41 +0400

    remove unnecessarry components and dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function RootLayout({
   children,
@@ -19,9 +8,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
